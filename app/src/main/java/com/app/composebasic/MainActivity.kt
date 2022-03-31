f9f0676f2ab5aaf7e866adf68b7a6da8bb6cca0f
정민혁 <jlssteeve1@gmail.com>
2022-03-31 20:39:20 +0900

    [UPDATE] Composable 함수 위치 옮김

--- app/src/main/java/com/app/composebasic/MainActivity.kt
+++ app/src/main/java/com/app/composebasic/MainActivity.kt
@@ -3,45 +3,31 @@ package com.app.composebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.app.composebasic.ui.theme.ComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
+    // ComposeBasic은 프로젝트 명이며 프로젝트 명 + Theme이라는 이름으로 생성된다.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // ComposeBasic은 프로젝트 명이며 프로젝트 명 + Theme이라는 이름으로 생성된다.
-            ComposeBasicTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            GreetingButton()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
-    }
-}
+
+// Amulator를 실행하기 전에 Split 또는 Design을 통해
+// 미리 실행화면을 볼 수 있는 함수이다.
+// DefaultPreview()는 테스트 코드같은 느낌이다.
+// 이를 삭제하면 코드나 디자인 보기 유형이 제거된다.
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeBasicTheme {
-        Greeting("ZzukMa")
+        GreetingButton()
     }
 }
